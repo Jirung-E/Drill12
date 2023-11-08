74059cccd8e2338cc7745ca0e04fa89f66cc9804
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2023-11-08 16:46:50 +0900

    좀비가 공에 맞으면 작아짐. 한번 ㅈ더 맞으면 없어짐

--- play_mode.py
+++ play_mode.py
@@ -38,6 +38,11 @@ def init():
     for ball in balls:
         game_world.add_collision_pair("boy:ball", None, ball)
 
+    zombies = [ Zombie() for _ in range(5) ]
+    game_world.add_objects(zombies, 1)
+    for zombie in zombies:
+        game_world.add_collision_pair("zombie:ball", zombie, None)
+
 
 def finish():
     game_world.clear()
--- zombie.py
+++ zombie.py
@@ -1,6 +1,7 @@
 import random
 import math
 import game_framework
+import game_world
 
 from pico2d import *
 
@@ -32,6 +33,7 @@ class Zombie:
         self.load_images()
         self.frame = random.randint(0, 9)
         self.dir = random.choice([-1,1])
+        self.size = 200
 
 
     def update(self):
@@ -47,11 +49,24 @@ class Zombie:
 
     def draw(self):
         if self.dir < 0:
-            Zombie.images['Walk'][int(self.frame)].composite_draw(0, 'h', self.x, self.y, 200, 200)
+            Zombie.images['Walk'][int(self.frame)].composite_draw(0, 'h', self.x, self.y, self.size, self.size)
         else:
-            Zombie.images['Walk'][int(self.frame)].draw(self.x, self.y, 200, 200)
+            Zombie.images['Walk'][int(self.frame)].draw(self.x, self.y, self.size, self.size)
+        draw_rectangle(*self.get_bb())
 
 
     def handle_event(self, event):
         pass
+
+    def get_bb(self):
+        w = self.size / 2.0
+        return (self.x-w, self.y-w, self.x+w, self.y+w)
+    
+    def handle_collision(self, group, other):
+        if group == "zombie:ball":
+            if self.size == 200:
+                self.size = 100
+                self.y -= 50
+            else:
+                game_world.remove_object(self)
 
