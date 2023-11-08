9c86e185113d9e19cde3c3246dff429a29dbe67d
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2023-11-08 16:49:21 +0900

    boy와 zombie가 닿으면 게임 종료

--- play_mode.py
+++ play_mode.py
@@ -40,8 +40,10 @@ def init():
 
     zombies = [ Zombie() for _ in range(5) ]
     game_world.add_objects(zombies, 1)
+    game_world.add_collision_pair("boy:zombie", boy, None)
     for zombie in zombies:
         game_world.add_collision_pair("zombie:ball", zombie, None)
+        game_world.add_collision_pair("boy:zombie", None, zombie)
 
 
 def finish():
